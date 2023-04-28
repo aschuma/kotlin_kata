816d2f7b0ae764c6e6bd46a6ea8b22d9fdae6864
aschuma <arno.schumacher@avono.de>
2023-04-28 18:53:47 +0200

    DSL 1.2.0 - functional errors

---
+++ dsl/src/test/kotlin/aschuma/kotlin/kata/dsl/FunctionalErrorTest1.kt
@@ -0,0 +1,54 @@
+package aschuma.kotlin.kata.dsl
+
+import arrow.core.*
+import arrow.core.raise.*
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.fail
+
+@Suppress("UNUSED_VARIABLE")
+class FunctionalErrorTest1 {
+
+   object UserNotFound
+   data class User(val id: Long)
+
+   private fun user(): User = User(1)
+
+   @Test
+   fun `learning fp error handling 1_2_0 - right`() {
+
+      // given/when
+      val userE: Either<UserNotFound, User> = either { user() }
+
+      // then
+      userE shouldBe user().right()
+      when (userE) {
+         is Either.Left -> fail("Unexpected 1")
+         is Either.Right -> userE.value shouldBe User(1)
+      }
+
+      // given/when/then
+      fold(block = { user() },
+         recover = { _: UserNotFound -> fail("Unexpected 2") },
+         transform = { u: User -> u shouldBe User(1) })
+   }
+
+   @Test
+   fun `learning fp error handling 1_2_0 - left`() {
+
+      // given/when
+      val userE: Either<UserNotFound, User> = either { raise(UserNotFound) }
+
+      // then
+      userE shouldBe UserNotFound.left()
+      when (userE) {
+         is Either.Left -> userE.value shouldBe UserNotFound
+         is Either.Right -> fail("Unexpected 3")
+      }
+
+      // given/when/then
+      fold(block = { raise(UserNotFound) },
+         recover = { u: UserNotFound -> u },
+         transform = { u: User -> fail("Unexpected 4") })
+   }
+}
---
+++ dsl/src/test/kotlin/aschuma/kotlin/kata/dsl/FunctionalErrorTest2.kt
@@ -0,0 +1,93 @@
+package aschuma.kotlin.kata.dsl
+
+import arrow.core.*
+import arrow.core.left
+import arrow.core.right
+import arrow.core.raise.*
+import io.kotest.matchers.equals.shouldBeEqual
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.fail
+
+@Suppress("UNUSED_VARIABLE")
+class FunctionalErrorTest2 {
+
+   data class User(val id: Long)
+   data class UserNotFound(val message: String = "Problem")
+
+   private fun User.isValid(): Either<UserNotFound, Unit> = either {
+      ensure(id > 0) { UserNotFound("User without a valid id: $id") }
+   }
+
+   private fun Raise<UserNotFound>.isValid(user: User): User {
+      ensure(user.id > 0) { UserNotFound("User without a valid id: ${user.id}") }
+      return user
+   }
+
+   private fun user(): User = User(1)
+
+   @Test
+   fun `learning fp error handling 1_2_0 - part 1`() {
+
+      // given/when/then
+      User(-1).isValid() shouldBe UserNotFound("User without a valid id: -1").left()
+
+      fold(block = {
+         val self: Raise<UserNotFound> = this
+         isValid(User(1))
+      }, recover = { _: UserNotFound -> fail("Unexpected 1") }, transform = { user: User -> user.id shouldBe 1 })
+   }
+
+   @Test
+   fun `learning fp error handling 1_2_0 - part 2`() {
+
+      // given/when/then
+      fun process(user: User?): Either<UserNotFound, Long> = either {
+         ensureNotNull(user) { UserNotFound("Cannot process null user") }
+         user.id // smart-casted to non-null
+      }
+      process(null) shouldBe UserNotFound("Cannot process null user").left()
+
+      // given/when/then
+      fun Raise<UserNotFound>.process(user: User?): Long {
+         ensureNotNull(user) { UserNotFound("Cannot process null user") }
+         return user.id // smart-casted to non-null
+      }
+      fold({ this.process(User(1L)) }, { _: UserNotFound -> fail("Unexpected ") }, { i: Long -> i shouldBe 1L })
+   }
+
+   @Test
+   fun `learning fp error handling 1_2_0 - nested`() {
+
+      fun problematic(n: Int): Either<UserNotFound, Int?> = either {
+
+         val eitherSelf = this
+
+         val intOrNull: Int? = nullable {
+
+            val nullableSelf: NullableRaise = this
+            when {
+               n < 0 -> eitherSelf.raise(UserNotFound())
+               n == 0 -> nullableSelf.raise(null)
+               n == 1 -> raise(null)
+               else -> n
+            }
+         }
+         intOrNull
+      }
+
+      problematic(-1) shouldBeEqual UserNotFound().left()
+      problematic(0) shouldBeEqual null.right()
+      problematic(1) shouldBeEqual null.right()
+      problematic(2) shouldBeEqual 2.right()
+   }
+
+   @Test
+   fun `learning fp error handling 1_2_0 - recover`() {
+      recover({
+         raise(UserNotFound())
+      }) { _: UserNotFound -> null } shouldBe null
+
+      UserNotFound().left().getOrElse { null } shouldBe null
+   }
+}
