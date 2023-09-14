9ae7a782db318abb4cedcc8d31bb4a59c7d022eb
aschuma <arno.schumacher@avono.de>
2023-09-14 22:03:43 +0200

    Kotlin Conf 2023 - Coroutines Workshop - hotflow

---
+++ coroutinesworkshop/src/main/kotlin/aschuma/kotlin/kata/coroutinesworkshop/hotflow/NumbersStation.kt
@@ -0,0 +1,49 @@
+package aschuma.kotlin.kata.coroutinesworkshop.hotflow
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.launch
+import kotlin.random.Random
+
+
+class NumbersStation {
+   private val scope = CoroutineScope(SupervisorJob())
+
+   private val _numbers: MutableSharedFlow<Int> = MutableSharedFlow(replay = 5)
+   val numbers: SharedFlow<Int> get() = _numbers
+
+   fun beginBroadcasting() {
+      scope.launch {
+         while (true) {
+            _numbers.emit(getNewNumber())
+         }
+      }
+   }
+}
+
+suspend fun main() {
+   val station = NumbersStation()
+   station.beginBroadcasting()
+   // Nobody listening!
+   delay(5000)
+   coroutineScope {
+      launch {
+         // Some items have been dropped
+         station.numbers.collect {
+            println("Received $it")
+         }
+      }
+   }
+}
+
+
+suspend fun getNewNumber(): Int {
+   val number = Random.nextInt()
+   println("Generated $number")
+   delay(500)
+   return number
+}
