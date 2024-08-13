fc2ff034e017572afd102e05057dbdad5ae2d54b
aschuma <arno.schumacher@avono.de>
2024-08-13 14:12:21 +0200

    fp - compose - lift

--- fp/src/test/kotlin/aschuma/kotlin/kata/fp/FunCompTest.kt
+++ fp/src/test/kotlin/aschuma/kotlin/kata/fp/FunCompTest.kt
@@ -6,15 +6,15 @@ import org.junit.jupiter.api.Test
 
 class FunCompTest {
 
-   private val funs: List<(Int) -> Int> = listOf(
-      { x: Int -> x + 1 },
-      { x: Int -> x * 2 },
-      { x: Int -> x - 3 }
-   )
+   private val funs: List<(Int) -> Int> = listOf({ x: Int -> x + 1 }, { x: Int -> x * 2 }, { x: Int -> x - 3 })
 
    infix operator fun List<(Int) -> Int>.invoke(arg: Int) = map { f -> f(arg) }
 
    infix operator fun <A, B, C> ((B) -> C).times(g: (A) -> B): (A) -> C = { x -> this(g(x)) }
+
+   infix operator fun <A, B, C> ((B) -> C).times(fList: List<(A) -> B>): List<(A) -> C> = fList.map { f -> this * f }
+
+   fun ((Int) -> Int).lift(): (List<Int>) -> List<Int> = { xs: List<Int> -> xs.map(this) }
 
    @Test
    fun `simple test - list f app - A`() {
@@ -39,7 +39,7 @@ class FunCompTest {
    @Test
    fun `simple test - list f fold - C`() {
       // when
-      val vals: List<Int> = funs.fold(emptyList<Int>()) { acc, f ->
+      val vals: List<Int> = funs.fold(emptyList()) { acc, f ->
          acc + f(3)
       }
 
@@ -98,9 +98,28 @@ class FunCompTest {
          times4 * f
       }
 
-      val g1 = { z: Int -> compositeFuns1.map { f -> f(z) } }
+      // then
+      assertEquals(compositeFuns1(3), listOf(16, 24, 0))
+   }
+
+   @Test
+   fun `simple test - list f compose 3`() {
+      // given
+      val times4 = { x: Int -> x * 4 }
+
+      // when
+      val vals = times4 * funs invoke 4
 
       // then
-      assertEquals(g1(3), listOf(16, 24, 0))
+      assertEquals(vals, listOf(20, 32, 4))
+   }
+
+   @Test
+   fun `simple test - list f compose 4`() {
+      // when
+      val vals = { x: Int -> x * 4 }.lift()(funs invoke 4)
+
+      // then
+      assertEquals(vals, listOf(20, 32, 4))
    }
 }
